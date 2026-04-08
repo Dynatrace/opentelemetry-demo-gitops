9f2b4dc5300ae0931b209526638aad18420c81b6
SzczepkowskiJan <jan.szczepkowski@dynatrace.com>
2026-04-08 12:15:42 +0200

    feat: add flag triggering image provider failure problem (#2558)

--- image-provider/terraform/modules/image-processing/locals.tf
+++ image-provider/terraform/modules/image-processing/locals.tf
@@ -7,4 +7,10 @@ locals {
     accessories = local.accessories_file_list
   }
   name_prefix = "image-provider-${var.environment}"
+
+  # When set to true, this flag causes the image-provider Lambda to fail during image processing.
+  # The failure simulates losing IAM permission to read the DynamoDB table used to retrieve the bucket name,
+  # so the Lambda will throw an error when attempting that lookup.
+  # NOTE: This change is intended to apply to all environments.
+  image_provider_failure_enabled = false
 }
