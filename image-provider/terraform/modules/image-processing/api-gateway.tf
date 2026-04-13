b90b06e7a9b7f282e993d7c3e1e733926f5b8779
dczupryn-dt <dezydery.czuprynski@dynatrace.com>
2026-04-13 12:29:59 +0200

    Ingest AWS API Gateway v2 logs to Dynatrace (#2612)
    * Add access logs
    
    * fmt
    
    * API-GatewayV2-Access-Logs*

--- image-provider/terraform/modules/image-processing/api-gateway.tf
+++ image-provider/terraform/modules/image-processing/api-gateway.tf
@@ -24,10 +24,36 @@ resource "aws_apigatewayv2_route" "this" {
   target    = "integrations/${aws_apigatewayv2_integration.this.id}"
 }
 
+# Log group naming convention: API-GatewayV2-Access-Logs*
+# Source: https://docs.dynatrace.com/docs/ingest-from/amazon-web-services/ingest-telemetry/cloudwatch-logs#supplogsources
+resource "aws_cloudwatch_log_group" "api_gw_access" {
+  name              = "API-GatewayV2-Access-Logs-${aws_apigatewayv2_api.this.name}"
+  retention_in_days = 7
+}
+
 resource "aws_apigatewayv2_stage" "this" {
   api_id      = aws_apigatewayv2_api.this.id
   name        = "default"
   auto_deploy = true
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw_access.arn
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      ip                      = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      httpMethod              = "$context.httpMethod"
+      routeKey                = "$context.routeKey"
+      path                    = "$context.path"
+      status                  = "$context.status"
+      protocol                = "$context.protocol"
+      responseLength          = "$context.responseLength"
+      responseLatency         = "$context.responseLatency"
+      integrationLatency      = "$context.integrationLatency"
+      integrationStatus       = "$context.integrationStatus"
+      errorMessage            = "$context.error.message"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+    })
+  }
 }
 
 resource "aws_lambda_permission" "apigw_invoke" {
